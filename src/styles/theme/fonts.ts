ba2b19a8b27d1dce4596ebc75ee16e69cce1f76f
Drew Beller <drew@spanninglabs.com>
2022-11-17 19:18:15 -0800

    make roboto-mono more universal

--- src/styles/theme/fonts.ts
+++ src/styles/theme/fonts.ts
@@ -1,6 +1,6 @@
 import type { DeepPartial, Theme } from "@chakra-ui/react";
 
 export const fonts: DeepPartial<Theme["fonts"]> = {
-  body: "'Roboto-mono', sans-serif",
+  body: "'Roboto-mono', serif",
   heading: "'Roboto-mono', serif",
 };
